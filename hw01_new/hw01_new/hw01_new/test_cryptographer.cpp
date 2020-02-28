378632194ddb4d5fcf9475928eac84f39e7a23ed
Vojtech Bumba <456238@mail.muni.cz>
2020-02-29 00:15:37 +0100

    tests

--- hw01_new/hw01_new/hw01_new/test_cryptographer.cpp
+++ hw01_new/hw01_new/hw01_new/test_cryptographer.cpp
@@ -3,13 +3,83 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("sha 512 test vector") {
+TEST_CASE("sha 512 test") {
+	std::vector<unsigned char> theKey{ '1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8' };
+
+	Cryptographer crypto(theKey);
+		
+	std::string hashedFile = crypto.hashFile("test.txt");
+	CHECK(crypto.compareHashToFile(hashedFile, "test.txt") == 1);
+	CHECK(crypto.compareHashFileToFile("test2.txt", "test2.txt") == 1);
+	CHECK(crypto.compareHashToFile(hashedFile, "test2.txt") == 0);
+	CHECK(crypto.compareHashFileToFile("test.txt", "test2.txt") == 0);
+}
+
+TEST_CASE("aes 128 cbc test file with content len ANY*16bytes") {
 	std::vector<unsigned char> theKey{ '1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8' };
 
 	Cryptographer crypto(theKey);
 
-	std::string input = "abc"; 
-	std::string hashedInput = crypto.hashFile(input);
+	std::ifstream input("test.txt");
+	std::stringstream ss;
+	while (auto chr = input.get()) {
+		ss << chr;
+	}
+	std::string content = ss.str();
+	
+	crypto.encryptFile("test.txt", "out.txt");
+	crypto.decryptFile("out.txt", "dec.txt");
 
-	CHECK(hashedInput == "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f");
+	std::ifstream dec("dec.txt");
+	std::stringstream ds;
+	while (auto chr = dec.get()) {
+		ds << chr;
+	}
+	CHECK(content == ds.str());
+}
+
+TEST_CASE("aes 128 cbc test file with content not len ANY*16bytes") {
+	std::vector<unsigned char> theKey{ '1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8' };
+
+	Cryptographer crypto(theKey);
+
+	std::ifstream input("test2.txt");
+	std::stringstream ss;
+	while (auto chr = input.get()) {
+		ss << chr;
+	}
+	std::string content = ss.str();
+
+	crypto.encryptFile("test2.txt", "out2.txt");
+	crypto.decryptFile("out2.txt", "dec2.txt");
+
+	std::ifstream dec("dec2.txt");
+	std::stringstream ds;
+	while (auto chr = dec.get()) {
+		ds << chr;
+	}
+	CHECK(content == ds.str());
+}
+
+TEST_CASE("aes 128 cbc test file with content shorter than 16bytes") {
+	std::vector<unsigned char> theKey{ '1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8' };
+
+	Cryptographer crypto(theKey);
+
+	std::ifstream input("test3.txt");
+	std::stringstream ss;
+	while (auto chr = input.get()) {
+		ss << chr;
+	}
+	std::string content = ss.str();
+
+	crypto.encryptFile("test3.txt", "out3.txt");
+	crypto.decryptFile("out3.txt", "dec3.txt");
+
+	std::ifstream dec("dec3.txt");
+	std::stringstream ds;
+	while (auto chr = dec.get()) {
+		ds << chr;
+	}
+	CHECK(content == ds.str());
 }
